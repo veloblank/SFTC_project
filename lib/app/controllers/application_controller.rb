85c356e580a80d34621a4b3f9fa2899bbc73ba0e
John <veloblank@gmail.com>
2017-09-18 16:24:48 -0400

    Added sessions.

--- lib/app/controllers/application_controller.rb
+++ lib/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require_relative '..config/environment'
 class App < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :views, "app/views"
     set :public_dir, "public"
   end
